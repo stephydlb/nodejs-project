9867d74f990da9f5a5cc30b6ff0a5cd5ca7509f1
stephydlb <stephkalubiaka@gmail.com>
2024-09-26 13:14:34 +0100

    configuration de route

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser') // ceci permet de recuperer les donnees 
 var server = express();
 
 // Body parser configuration
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true })); // pour recuperer les donnees du body de la requette http
+server.use(bodyParser.json()); // server.use permet de configurer le serveur et bodyParser.json permet de recuperer les donnees du body de la requette http 
 
 // configure routes
 server.get('/', function (req, res) {
